f419a1271183e7bb019b5099aa05e409d5c09912
Lucas Carvalho <ribeirolucas149@gmail.com>
2021-04-14 22:39:15 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,6 +57,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!!!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -98,7 +101,6 @@ public class UI {
 	}
 	
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		//expressão lambda
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		System.out.println("Captured pieces");
